2dab3694fbb979ab6575c7fbe914e820673cf3e1
hoffcamp <hoffcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-15 05:31:14 +0000

    removed extra 'trunk' directory
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-form-manager/tags/1.1@373152 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/js/helpers.js
+++
@@ -1,9 +0,0 @@
-function fm_trim(str){
-	return str.replace(/^\s+|\s+$/g,"");
-}
-function fm_fix_str(str){
-	return str.replace(/[\\]/g,'\\\\\\\\').replace(/[']/g,'\\\\\\$&');
-}
-function fm_htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
