8f76c3356b659daf7ade8ce05900172afac05972
hoffcamp <hoffcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-25 02:35:27 +0000

    tagged 1.4.14
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-form-manager/tags/1.4.14@388752 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- types/textarea.php
+++ types/textarea.php
@@ -18,10 +18,10 @@ class fm_textareaControl extends fm_controlBase{
 	
 	public function itemDefaults(){
 		$itemInfo = array();
-		$itemInfo['label'] = "Item Label";
+		$itemInfo['label'] = "New Text Area";
 		$itemInfo['description'] = "Item Description";
 		$itemInfo['extra'] = array('cols'=>'300', 'rows' => '100');
-		$itemInfo['nickname'] = "Item Nickname";
+		$itemInfo['nickname'] = '';
 		$itemInfo['required'] = 0;
 		$itemInfo['validator'] = "";
 		$ItemInfo['validation_msg'] = "";
