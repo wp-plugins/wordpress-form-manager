13461d7e055002eb0162a0adbc7e08d28ee47cf7
hoffcamp <hoffcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-08 17:52:04 +0000

    tagged 1.5.6
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-form-manager/tags/1.5.6@394659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/editformadv.php
+++ pages/editformadv.php
@@ -26,12 +26,8 @@ if(isset($_POST['submit-form-settings'])){
 	$formInfo['advanced_email'] = $_POST['advanced_email'];
 	$formInfo['publish_post'] = ($_POST['publish_post']=="on"?1:0);
 	$formInfo['publish_post_category'] = $_POST['publish_post_category'];
-	$formInfo['publish_post_title'] = $_POST['publish_post_title'];
+	$formInfo['publish_post_title'] = $_POST['publish_post_title'];	
 	
-	$formInfo['items'] = $form['items'];
-	foreach($form['items'] as $index => $item){
-		$formInfo['items'][$index]['nickname'] = sanitize_title($_POST[$item['unique_name'].'-nickname']);		
-	}
 	$fmdb->updateForm($_POST['fm-form-id'], $formInfo);
 }
 
@@ -69,19 +65,14 @@ $fm_globalSettings = $fmdb->getGlobalSettings();
 <input type="hidden" value="<?php echo $form['ID'];?>" name="fm-form-id" />
 
 <div class="wrap">
-<div id="icon-edit-pages" class="icon32"></div>
-<?php /* translators: This specifies the 'advanced' settings pages */ ?>
-<h2><?php echo $form['title'];?> - <?php _e("Advanced", 'wordpress-form-manager');?></h2>
 
 <div style="float:right;">
+<input type="submit" name="cancel" class="button secondary" value="<?php _e("Cancel Changes", 'wordpress-form-manager');?>" />
 <input type="submit" name="submit-form-settings" id="submit" class="button-primary" value="<?php _e("Save Changes", 'wordpress-form-manager');?>"  />&nbsp;&nbsp;
-<?php if(!$fm_MEMBERS_EXISTS || current_user_can('form_manager_forms')): ?>
-<a class="preview button" href="<?php echo get_admin_url(null, 'admin.php')."?page=fm-edit-form&id=".$form['ID'];?>" ><?php _e("Edit Form", 'wordpress-form-manager');?></a>
-<?php endif; ?>
 </div>
 
 	<div id="message-container"><?php 
-	if(isset($_POST['message']))
+	if(isset($_POST['message']) && isset($_POST['submit-form-settings']))
 		switch($_POST['message']){
 			case 1: ?><div id="message-success" class="updated"><p><strong><?php _e("Settings Saved.", 'wordpress-form-manager');?> </strong></p></div><?php break;
 			case 2: ?><div id="message-error" class="error"><p><?php _e("Save failed.", 'wordpress-form-manager');?> </p></div><?php break;
@@ -120,19 +111,6 @@ helper_option_field('summary_template', __("Data Summary", 'wordpress-form-manag
 </table>
 <textarea name="advanced_email" rows="15" style="width:80%" ><?php echo $form['advanced_email']; ?></textarea>
 
-<h3><?php _e("Form Item Nicknames", 'wordpress-form-manager');?></h3>
-<table>
-<tr><td colspan="2"><span class="description"><?php _e("Giving a nickname to form items makes it easier to access their information within custom e-mail notifications and templates", 'wordpress-form-manager');?></span></td></tr>
-</table>
-<br />
-<table class="form-table">
-<tr><th><strong><?php _e("Item Label", 'wordpress-form-manager');?></strong></th><th><strong><?php _e("Nickname", 'wordpress-form-manager');?></strong></th></tr>
-<?php foreach($form['items'] as $item){
-	if($item['type'] != 'separator' && $item['type'] != 'note' && $item['type'] != 'recaptcha')
-		helper_text_field($item['unique_name'].'-nickname', $item['label'], $item['nickname']);
-} ?>
-</table>
-
 <h3><?php _e("Publish Submitted Data", 'wordpress-form-manager');?></h3>
 <table class="form-table">
 <?php helper_checkbox_field('publish_post', __("Publish submissions as posts", 'wordpress-form-manager'), ($form['publish_post'] == 1)); ?> 
@@ -140,7 +118,10 @@ helper_option_field('summary_template', __("Data Summary", 'wordpress-form-manag
 <?php helper_text_field('publish_post_title', __("Post title", 'wordpress-form-manager'), htmlspecialchars($form['publish_post_title']), __("Include '%s' where you would like the form title to appear", 'wordpress-form-manager')); ?>
 </table>
 
-<p class="submit"><input type="submit" name="submit-form-settings" id="submit" class="button-primary" value="<?php _e("Save Changes", 'wordpress-form-manager');?>"  /></p>
+<p class="submit">
+<input type="submit" name="cancel" class="button secondary" value="<?php _e("Cancel Changes", 'wordpress-form-manager');?>" />
+<input type="submit" name="submit-form-settings" id="submit" class="button-primary" value="<?php _e("Save Changes", 'wordpress-form-manager');?>"  />
+</p>
 
 </div>
 
