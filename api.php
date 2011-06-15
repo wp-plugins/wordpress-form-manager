dbcf71b7048e25a965c963ff756ad803f38e7517
hoffcamp <hoffcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-15 09:06:50 +0000

    tagged 1.5.14
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-form-manager/tags/1.5.14@397350 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api.php
+++ api.php
@@ -2,8 +2,14 @@
 /**************************************************************/
 /******* API **************************************************/
 
-//takes a form's slug as a string, returns paginated 
-function fm_doDataListBySlug($formSlug, $template, $orderBy = 'timestamp', $ord = 'DESC', $dataPerPage = 30){
+//the attributes that can be specified per item.  The attribute takes the name (item nickname)_(att)
+// so width becomes (item nickname)_width, with a default value of 'auto'.
+$fm_tablePerItemAttributes = array(
+	'width' => 'auto',
+	'class' => '',
+);
+
+function fm_doPaginatedSummariesBySlugCallback($formSlug, $template, $callback, $orderBy = 'timestamp', $ord = 'DESC', $dataPerPage = 30, $options=array()){
 	global $fmdb;
 	
 	parse_str($_SERVER['QUERY_STRING'], $queryVars);
@@ -53,8 +59,7 @@ function fm_doDataListBySlug($formSlug, $template, $orderBy = 'timestamp', $ord 
 	}
 	
 	// summaries
-	$summaries = fm_getFormDataSummaries($formID, $template, $orderBy, $ord, $currentStartIndex, $dataPerPage);
-	$summaryListStr = '<p class="fm-data">'.implode('</p><p class="fm-data">', $summaries).'</p>';
+	$summaryListStr = $callback($formID, $template, $orderBy, $ord, $currentStartIndex, $dataPerPage, $options);
 	
 	// put it all together
 	return  $pageLinkStr.
@@ -62,7 +67,139 @@ function fm_doDataListBySlug($formSlug, $template, $orderBy = 'timestamp', $ord 
 			$pageLinkStr;
 }
 
-//takes a form's slug as a string, returns an array of strings containing formatted data summaries, using the 'summary' template.
+function fm_doDataTableBySlug($formSlug, $template, $orderBy = 'timestamp', $ord = 'DESC', $dataPerPage = 30, $options=array()){
+	return fm_doPaginatedSummariesBySlugCallback($formSlug, $template, 'fm_getFormDataTable', $orderBy, $ord, $dataPerPage, $options);
+}
+
+function fm_getFormDataTable($formID, $template, $orderBy = 'timestamp', $ord = 'DESC', $startIndex = 0, $numItems = 30, $options=array()){
+	global $fmdb;
+	global $fm_display;
+	global $fm_controls;
+	
+	$showcols = isset($options['show']) ? explode(',', $options['show']) : false;
+	$hidecols = isset($options['hide']) ? explode(',', $options['hide']) : false;
+	
+	$formInfo = $fmdb->getForm($formID);
+	$formData = $fmdb->getFormSubmissionData($formID, $orderBy, strtoupper($ord), $startIndex, $numItems);
+	$atts = fm_helper_extractColumnAtts($formInfo, $options);
+	$hasPosts = $fmdb->dataHasPublishedSubmissions($formInfo['ID']);
+	
+	$str = "";
+//	$str .= '<pre>'.print_r($options,true).'</pre>';
+//	$str .= '<pre>'.print_r($atts,true).'</pre>';
+	$str .= '<table class="fm-data">';
+	
+	$tbllbl = '<tr>';
+		$class = "";
+		if(isset($options['col_class']))
+			$class = $options['col_class'];
+		
+		$universalCols = array(
+			'timestamp' => __("Timestamp", 'wordpress-form-manager'), 
+			'user' => __("User", 'wordpress-form-manager'),
+			'post' => __("Post", 'wordpress-form-manager'),
+		);
+		
+		if($hasPosts === false) unset($universalCols['post']);
+		
+		foreach($universalCols as $col => $lbl){
+			if (fm_helper_is_shown_col($showcols, $hidecols, $col)){
+				$tbllbl.= '<th class="fm-item-header-'.$col.'" style="width:'.(isset($atts[$col.'_width']) ? $atts[$col.'_width'] : $atts['col_width']).';" ';
+				$tmp = $class.' '.$atts[$col.'_class'];
+				if(trim($tmp) != "") {
+					$tbllbl.= ' class="'.$tmp.'"';
+				}
+				$tbllbl.= '>'.$lbl.'</th>';
+			}
+		}	
+		
+		foreach($formInfo['items'] as $item){
+			if($fmdb->isDataCol($item['unique_name'])){
+				$lbl = ($item['nickname'] != "") ? $item['nickname'] : $item['unique_name'];				
+				if (fm_helper_is_shown_col($showcols, $hidecols, $lbl)) {			
+						$width = ' style="width:'.$atts[$item['nickname'].'_width'].';"';	
+						$tbllbl.= '<th class="fm-item-header-'.$lbl.'"'.$width.'>'.$item['nickname'].'</th>';
+				}
+			}
+		}
+	$tbllbl.= '</tr>';
+	
+	$str.= '<thead>'.$tbllbl.'</thead>';
+	$str.= '<tfoot>'.$tbllbl.'</tfoot>';
+	
+	foreach($formData['data'] as $dataRow){
+		$height = (isset($options['row_height'])) ? ' style="height:'.$options['row_height'].';"' : '';
+		$class = (isset($options['row_class'])) ? ' class="'.$options['row_class'].'"' : '';
+			
+		$str .= '<tr'.$height.$class.'>';
+		
+		if(fm_helper_is_shown_col($showcols, $hidecols, 'timestamp'))
+			$str.= '<td class="fm-item-cell-timestamp">'.$dataRow['timestamp'].'</td>';
+		if(fm_helper_is_shown_col($showcols, $hidecols, 'user'))
+			$str.= '<td class="fm-item-cell-user">'.$dataRow['user'].'</td>';
+		if($hasPosts && fm_helper_is_shown_col($showcols, $hidecols, 'post')){
+			if($dataRow['post_id'] > 0)
+				$str.= '<td class="fm-item-cell-post"><a href="'.get_permalink($dataRow['post_id']).'">'.get_the_title($dataRow['post_id']).'</a></td>';
+			else
+				$str.= '<td class="fm-item-cell-post">&nbsp;</td>';
+		}
+			
+		foreach($formInfo['items'] as $item){
+			$lbl = ($item['nickname'] != "") ? $item['nickname'] : $item['unique_name'];
+			if($fmdb->isDataCol($item['unique_name']) && fm_helper_is_shown_col($showcols, $hidecols, $lbl)){				
+				$tmp = $dataRow[$item['unique_name']];
+				if($item['type'] == 'file')	
+					$str.= '<td class="fm-item-cell-'.$lbl.'">'.$tmp.'</td>';
+				else
+					$str.= '<td class="fm-item-cell-'.$lbl.'">'.fm_restrictString($tmp, 75).'</td>';
+			}		
+		}
+		$str.= '</tr>';
+	}
+
+	$str.= '</table>';
+	
+	return $str;
+}
+
+function fm_helper_extractColumnAtts($formInfo, $options){
+	global $fm_tablePerItemAttributes;
+	$colAtts = array();
+	
+	foreach($fm_tablePerItemAttributes as $att => $val){
+		$colAtts['timestamp_'.$att] = $val;
+		$colAtts['user_'.$att] = $val;
+		$colAtts['post_'.$att] = $val;
+	}
+			
+	foreach($formInfo['items'] as $item){
+		if($item['nickname'] != ""){
+			foreach($fm_tablePerItemAttributes as $att => $val){
+				$colAtts[$item['nickname'].'_'.$att] = $val;
+			}
+		}
+	}
+	
+	$atts = shortcode_atts( $colAtts, $options );
+	return $atts;
+}
+
+function fm_helper_is_shown_col($showcols, $hidecols, $lbl){
+	$lbl = trim($lbl);
+	if (($showcols !== false && in_array($lbl, $showcols))
+		|| ($hidecols !== false && !in_array($lbl, $hidecols))
+		|| ($showcols === false && $hidecols === false)) {
+		return true;
+	}
+	return false;
+}
+
+//takes a form's slug as a string, returns paginated 
+function fm_doDataListBySlug($formSlug, $template, $orderBy = 'timestamp', $ord = 'DESC', $dataPerPage = 30){
+	return fm_doPaginatedSummariesBySlugCallback($formSlug, $template, 'fm_getFormDataSummaries', $orderBy, $ord, $dataPerPage);
+}
+
+//takes a form's slug as a string, returns formatted data summaries, using the 'summary' template.
 function fm_getFormDataSummaries($formID, $template, $orderBy = 'timestamp', $ord = 'DESC', $startIndex = 0, $numItems = 30){
 	global $fmdb;
 	global $fm_display;
@@ -71,13 +208,12 @@ function fm_getFormDataSummaries($formID, $template, $orderBy = 'timestamp', $or
 	
 	$formData = $fmdb->getFormSubmissionDataRaw($formID, $orderBy, strtoupper($ord), $startIndex, $numItems);
 	
-	
 	$strArray = array();
 	foreach($formData as $dataRow){
 		$strArray[] = $fm_display->displayDataSummary($template, $formInfo, $dataRow);
 	}
 	
-	return $strArray;	
+	return '<p class="fm-data">'.implode('</p><p class="fm-data">', $strArray).'</p>';
 }
 
 function fm_getFormID($formSlug){
@@ -86,128 +222,100 @@ function fm_getFormID($formSlug){
 }
 
 //takes a form's slug as a string.  It has the same behavior as using the shortcode.  Displays the form (according to the set behavior), processes posts, etc.
-function fm_doFormBySlug($formSlug){
+
+
+function fm_doFormBySlug($formSlug, $options = array()){
 	global $fm_display;
 	global $fmdb;
 	global $current_user;
 	global $fm_registered_user_only_msg;
-		
-	$formID = $fmdb->getFormID($formSlug);
-	if($formID === false) return "(form ".(trim($formSlug)!=""?"'{$formSlug}' ":"")."not found)";
 	
+	
+	// error checking
+	$formID = $fmdb->getFormID($formSlug);
+	if($formID === false) return sprintf(__("(form  %s not found)", 'wordpress-form-manager'), (trim($formSlug)!=""?"'{$formSlug}' ":""));
+		
+	if(isset($formBehaviors['reg_user_only']) && $current_user->user_login == "") 
+		return sprintf($fm_registered_user_only_msg, $formInfo['title']);
+		
 	$output = "";
 	
-	//get and parse the form settings
 	$formInfo = $fmdb->getForm($formID);
-	$arr = explode(",", $formInfo['behaviors']);
-	$formBehaviors = array();
-	foreach($arr as $v){
-		$formBehaviors[$v] = true;
-	}
 	
+	$formBehaviors = fm_helper_parseBehaviors($formInfo['behaviors']);
+		
 	$userDataCount = $fmdb->getUserSubmissionCount($formID, $current_user->user_login);
 	
-	if($_POST['fm_id'] == $formID && (wp_verify_nonce($_POST['fm_nonce'],'fm-nonce') && ($userDataCount == 0 || !isset($formBehaviors['single_submission'])))){
+	//process the data submission
+	if($_POST['fm_id'] == $formID 
+		&& (wp_verify_nonce($_POST['fm_nonce'],'fm-nonce') 
+			&& ($userDataCount == 0 || !isset($formBehaviors['single_submission']))
+			)
+		){
 		// process the post
 		get_currentuserinfo();	
 		
 		$overwrite = (isset($formBehaviors['display_summ']) || isset($formBehaviors['overwrite']));
-		$postData = $fmdb->processPost($formID, array('user'=>$current_user->user_login, 'user_ip' => fm_get_user_IP()), $overwrite);			
-		foreach($formInfo['items'] as $item){
-			if($item['type'] != 'file')
-				$postData[$item['unique_name']] = stripslashes($postData[$item['unique_name']]);
+		
+		$postData = $fmdb->processPost(
+			$formID,
+			array('user'=>$current_user->user_login,
+				'user_ip' => fm_get_user_IP(),
+				'unique_id' => $_POST['fm_unique_id'],
+				), 
+			$overwrite
+			);			
+		
+		//strip slashes in case we need to display the submitted data
+		foreach($formInfo['items'] as $item){			
+			$postData[$item['unique_name']] = stripslashes($postData[$item['unique_name']]);
 		}
 			
 		if($fmdb->processFailed()){			
 			return '<em>'.$fmdb->getErrorMessage().'</em>'.
 					$fm_display->displayForm($formInfo, array('action' => get_permalink(), 'use_placeholders' => false), $postData);
 		}
-		else{
-			// send email notifications
-				
-			if($formInfo['use_advanced_email'] != 1){
-			
-				$formInfo['email_list'] = trim($formInfo['email_list']) ;
-				$formInfo['email_user_field'] = trim($formInfo['email_user_field']);		
-					
-				if($formInfo['email_list'] != ""
-				|| $formInfo['email_user_field'] != "" 
-				|| $fmdb->getGlobalSetting('email_admin') == "YES"
-				|| $fmdb->getGlobalSetting('email_reg_users') == "YES"){
-				
-					$subject = get_option('blogname').": '".$formInfo['title']."' Submission";				
-					$message = $fm_display->displayDataSummary('email', $formInfo, $postData);
-					$headers  = 'MIME-Version: 1.0'."\r\n".
-								'Content-type: text/html'."\r\n".
-								'From: '.get_option('admin_email')."\r\n".
-								'Reply-To: '.get_option('admin_email')."\r\n";
-					
-					$temp = "";
-					if($fmdb->getGlobalSetting('email_admin') == "YES")
-						wp_mail(get_option('admin_email'), $subject, $message, $headers);
-						
-					if($fmdb->getGlobalSetting('email_reg_users') == "YES"){
-						if(trim($current_user->user_email) != "")
-							wp_mail($current_user->user_email, $subject, $message, $headers);
-					}
-					if($formInfo['email_list'] != "")
-						wp_mail($formInfo['email_list'], $subject, $message, $headers);
-						
-					if($formInfo['email_user_field'] != "")
-						wp_mail($postData[$formInfo['email_user_field']], $subject, $message, $headers);
 		
-				}
-			}else{
-				//use the advanced e-mail settings 
-				$advEmail = new fm_advanced_email_class($formInfo, $postData);
+		// send email notifications
+			
+		if($formInfo['use_advanced_email'] != 1){
+			fm_helper_sendEmail($formInfo, $postData);			
+		}else{			
+			$advEmail = new fm_advanced_email_class($formInfo, $postData);
 
-				$emails = $advEmail->generateEmails($formInfo['advanced_email']);
-								
-				foreach($emails as $email){
-					$headerStr = "";
-					foreach($email['headers'] as $header => $value)
-						$headerStr.= $header.": ".$value."\r\n";
-					wp_mail($email['to'], $email['subject'], $email['message'], $headerStr);
-				}
+			$emails = $advEmail->generateEmails($formInfo['advanced_email']);
+							
+			foreach($emails as $email){
+				$headerStr = "";
+				foreach($email['headers'] as $header => $value)
+					$headerStr.= $header.": ".$value."\r\n";
+				wp_mail($email['to'], $email['subject'], $email['message'], $headerStr);
 			}
-			
-			//publish the submission as a post, if the form is set to do so
-			if($formInfo['publish_post'] == 1){
-				// Create post object
-				 $my_post = array(
-					 'post_title' => sprintf($formInfo['publish_post_title'], $formInfo['title']),
-					 'post_content' => $fm_display->displayDataSummary('summary', $formInfo, $postData),
-					 'post_status' => 'publish',
-					 'post_author' => 1,
-					 'post_category' => array($formInfo['publish_post_category'])
-				  );
-				
-				// Insert the post into the database
-				  wp_insert_post( $my_post );
-			}			
-			
-			//display the acknowledgment of a successful submission
-			$output.= '<p>'.$formInfo['submitted_msg'].'</p>';
-			
-			//show the automatic redirection script
-			if($formInfo['auto_redirect']==1){
-				$output.=	"<script language=\"javascript\"><!--\n".
-							"setTimeout('location.replace(\"".get_permalink($formInfo['auto_redirect_page'])."\")', ".($formInfo['auto_redirect_timeout']*1000).");\n"\.
-							"//-->\n".
-							"</script>\n";
-			}
-			
-			if(!isset($formBehaviors['display_summ']))
-				return $output.
-						($formInfo['show_summary']==1 ? $fm_display->displayDataSummary('summary', $formInfo, $postData) : "");
 		}
+		
+		//publish the submission as a post, if the form is set to do so
+		if($formInfo['publish_post'] == 1){				
+			fm_helper_publishPost($formInfo, $postData);
+		}			
+		
+		//display the acknowledgment of a successful submission
+		$output.= '<p>'.$formInfo['submitted_msg'].'</p>';
+		
+		//show the automatic redirection script
+		if($formInfo['auto_redirect']==1){
+			$output.=	"<script language=\"javascript\"><!--\n".
+						"setTimeout('location.replace(\"".get_permalink($formInfo['auto_redirect_page'])."\")', ".($formInfo['auto_redirect_timeout']*1000).");\n".
+						"//-->\n".
+						"</script>\n";
+		}
+		
+		//the 'display_summ' behavior means we show the summary *instead of* the form.  The 'show_summary' flag in formInfo means we show a summary *with* the form. Confusing.
+		if(!isset($formBehaviors['display_summ']))
+			return $output.
+					($formInfo['show_summary']==1 ? $fm_display->displayDataSummary('summary', $formInfo, $postData) : "");		
 	}
 		
-	//'reg_user_only', block unregistered users
-	if(isset($formBehaviors['reg_user_only']) && $current_user->user_login == "") 
-		return sprintf($fm_registered_user_only_msg, $formInfo['title']);
-		
-	//'display_summ', show previous submission if there is one and break
+	//'display_summ', show previous submission if there is one, instead of the form
 	
 	if(isset($formBehaviors['display_summ'])){
 		$userData = $fmdb->getUserSubmissions($formID, $current_user->user_login, true);
@@ -230,11 +338,74 @@ function fm_doFormBySlug($formSlug){
 				}				
 			}
 			else
-				return $output.$fm_display->displayForm($formInfo, array('action' => get_permalink(), 'use_placeholders' => false), $userData[0]);
+				return $output.$fm_display->displayForm($formInfo, array_merge($options, array('action' => get_permalink(), 'use_placeholders' => false)), $userData[0]);
 		}
 	}
 	
 	//if we got this far, just display the form
-	return $fm_display->displayForm($formInfo, array('action' => get_permalink()));
+	return $fm_display->displayForm($formInfo, array_merge($options, array('action' => get_permalink())));
+}
+
+function fm_helper_parseBehaviors($behaviorString){
+	$arr = explode(",", $behaviorString);
+	$formBehaviors = array();
+	foreach($arr as $v){
+		$formBehaviors[$v] = true;
+	}
+	return $formBehaviors;
+}
+function fm_helper_publishPost($formInfo, $postData){
+	global $fm_display;
+	global $fmdb;
+	
+	// Create post object
+	$newPost = array(
+		'post_title' => sprintf($formInfo['publish_post_title'], $formInfo['title']),
+		'post_content' => $fm_display->displayDataSummary('summary', $formInfo, $postData),
+		'post_status' => 'publish',
+		'post_author' => 1,
+		'post_category' => array($formInfo['publish_post_category'])
+	);
+	
+	// Insert the post into the database
+	$postID = wp_insert_post($newPost, false);
+	if($postID != 0){					
+		$fmdb->updateDataSubmissionRow($formInfo['ID'], $postData['timestamp'], $postData['user'], $postData['user_ip'], array('post_id' => $postID));
+	}
+}
+function fm_helper_sendEmail($formInfo, $postData){
+	global $fmdb;
+	global $current_user;
+	global $fm_display;
+	
+	$formInfo['email_list'] = trim($formInfo['email_list']) ;
+	$formInfo['email_user_field'] = trim($formInfo['email_user_field']);		
+		
+	if($formInfo['email_list'] != ""
+	|| $formInfo['email_user_field'] != "" 
+	|| $fmdb->getGlobalSetting('email_admin') == "YES"
+	|| $fmdb->getGlobalSetting('email_reg_users') == "YES"){
+	
+		$subject = get_option('blogname').": '".$formInfo['title']."' Submission";				
+		$message = $fm_display->displayDataSummary('email', $formInfo, $postData);
+		$headers  = 'MIME-Version: 1.0'."\r\n".
+					'Content-type: text/html'."\r\n".
+					'From: '.get_option('admin_email')."\r\n".
+					'Reply-To: '.get_option('admin_email')."\r\n";
+		
+		$temp = "";
+		if($fmdb->getGlobalSetting('email_admin') == "YES")
+			wp_mail(get_option('admin_email'), $subject, $message, $headers);
+			
+		if($fmdb->getGlobalSetting('email_reg_users') == "YES"){
+			if(trim($current_user->user_email) != "")
+				wp_mail($current_user->user_email, $subject, $message, $headers);
+		}
+		if($formInfo['email_list'] != "")
+			wp_mail($formInfo['email_list'], $subject, $message, $headers);
+			
+		if($formInfo['email_user_field'] != "")
+			wp_mail($postData[$formInfo['email_user_field']], $subject, $message, $headers);
+	}
 }
 ?>
---
+++ js/userscripts.js
@@ -0,0 +1,259 @@
+var fm_registered_form_items = [];
+var fm_registered_forms = [];
+
+/* PHP passes the form structure to JS using 'fm_register_form_item' and 'fm_register_form' */
+//itemDef is an 'associative array', the item's database entry (unpacked)
+function fm_register_form_item(formID, itemDef){
+	itemDef.formID = formID;
+	fm_registered_form_items.push(itemDef);
+}
+
+// returns the 'registered' information about the form element, essentially the DB entry
+function fm_get_form_item_info(itemID){
+	for(var x=0;x<fm_registered_form_items.length;x++){
+		if(fm_registered_form_items[x].unique_name == itemID
+			|| fm_registered_form_items[x].nickname == itemID){
+			return fm_registered_form_items[x];
+		}
+	}
+}
+
+//returns the form element (unless it is a checkbox/radio list)
+function fm_get_form_item(itemID){
+	var itemInfo = fm_get_form_item_info(itemID);
+	return document.getElementById('fm-form-' + itemInfo.ID)[itemInfo.unique_name];
+}
+
+//the same as the above, but for checkbox/radio lists
+function fm_get_form_item_indexed(itemID, index){
+	var itemInfo = fm_get_form_item_info(itemID);
+	return document.getElementById('fm-form-' + itemInfo.ID)[itemInfo.unique_name + '-' + index];
+}	
+
+//adds the appropriate event handlers to the form: 'fm_submit_onclick' becomes the onclick event handler for any item with the name 'fm_form_submit'
+function fm_register_form(formID){
+	var formElement = document.getElementById('fm-form-' + formID);
+	var submitButton = formElement['fm_form_submit'];
+	//submitButton.onclick = function(){ return fm_submit_onclick_ajax(formID, formSlug); }
+	submitButton.onclick = function(){ return fm_submit_onclick(formID); }
+	fm_registered_forms[formID] = false;
+}
+
+function fm_submit_onclick(formID){
+	if(!fm_check_required_items(formID)) return false;
+	if(!fm_check_text_validation(formID)) return false;
+	return true;
+}
+
+function fm_submit_onclick_ajax(formID, formSlug){
+	if(fm_registered_forms[formID]) return false;
+	fm_registered_forms[formID] = true;
+	
+	if(!fm_check_required_items(formID)) return false;
+	if(!fm_check_text_validation(formID)) return false;
+	
+	var data = {
+		action: 'fm_post_form',
+		slug: formSlug,
+		fm_id: document.getElementById('fm-form-' + formID)['fm_id'].value,
+		fm_nonce: document.getElementById('fm-form-' + formID)['fm_nonce'].value
+	};		
+	var temp;
+	
+	for(var x=0;x<fm_registered_form_items.length;x++){
+		if(fm_registered_form_items[x].formID == formID){
+			eval("temp = " + fm_registered_form_items[x].getter_script + "(\'" + formID + "\', \'" + fm_registered_form_items[x].unique_name + "\');");
+			data[fm_registered_form_items[x].unique_name] = temp;
+		}
+	}
+	
+	var ajaxurl = fm_user_I18n.ajaxurl;
+	
+	jQuery.post(ajaxurl, data, function(response){
+		var formEL = document.getElementById('fm-form-' + formID);
+		jQuery(formEL).after(response).remove();
+	});
+	
+	return false;
+}
+
+/* Validation checks */
+
+//check text validation
+function fm_check_text_validation(formID){
+	var msg = "";
+	for(var x=0;x<fm_registered_form_items.length;x++){
+		if(fm_registered_form_items[x].formID == formID && 
+			fm_registered_form_items[x].type == 'text' &&
+			!fm_item_validation_satisfied(fm_registered_form_items[x])){
+			if(!temp){
+				if(msg != "") msg += "\n";
+				msg += fm_registered_form_items[x].validation_msg;	
+			}
+		}
+	}
+	if(msg != ""){
+		alert(msg);
+		return false;
+	}
+	return true;
+}
+
+function fm_item_validation_satisfied(itemDef){
+	if(itemDef.validation_callback != ""){
+		eval("temp = " + itemDef.validation_callback + "('" + itemDef.formID + "', '" + itemDef.unique_name + "', '" + itemDef.validation_type + "');");
+		return temp;
+	}
+	return true;
+}
+
+
+
+//check required items
+function fm_check_required_items(formID){
+	var msg = "";
+	for(var x=0;x<fm_registered_form_items.length;x++){
+		if(fm_registered_form_items[x].formID == formID && 
+			!fm_item_required_satisfied(fm_registered_form_items[x])){
+			if(!temp){
+				if(msg != "") msg += "\n";
+				msg += fm_registered_form_items[x].required_msg;	
+			}
+		}
+	}
+	if(msg != ""){
+		alert(msg);
+		return false;
+	}
+	return true;
+}
+
+
+function fm_item_required_satisfied(itemDef){
+	if(itemDef.required == 1 && itemDef.required_callback != ""){
+		eval("temp = " + itemDef.required_callback + "('" + itemDef.formID + "', '" + itemDef.unique_name + "');");
+		return temp;
+	}
+	return true;
+}
+
+function fm_set_required(itemID, req){
+	for(var x=0;x<fm_registered_form_items.length;x++){
+		temp = fm_registered_form_items[x];
+		if(temp.unique_name == itemID){			
+			fm_registered_form_items[x].required = req;
+			tempID = 'fm-item-' + (temp.nickname != "" ? temp.nickname : temp.unique_name);
+			EMs = document.getElementById(tempID).getElementsByTagName('em');
+			EMs[0].style.display = (req == 1 ? 'inline' : 'none');
+		}
+	}
+	
+}
+/* HTML 5 support & simulation */
+
+function fm_supports_placeholder(){
+	placeholderSupport = ("placeholder" in document.createElement("input"));
+	return placeholderSupport;
+}
+
+function fm_add_placeholders(){
+	if(!fm_supports_placeholder()){
+		for(var i=0;i<fm_registered_form_items.length;i++){
+			if(fm_registered_form_items[i].type == 'text'){
+				var textItem = document.getElementById('fm-form-' + fm_registered_form_items[i].formID)[fm_registered_form_items[i].unique_name];
+				textItem.value = fm_registered_form_items[i].extra.value;
+				textItem.ph_hasEdit = false;
+				textItem.ph_thePlaceholder = fm_registered_form_items[i].extra.value;
+				textItem.onfocus = fm_simulate_placeholder_onfocus;
+				textItem.onblur = fm_simulate_placeholder_onblur;
+				textItem.onchange = fm_simulate_placeholder_onchange;
+			}
+		}
+	}
+}
+
+function fm_simulate_placeholder_onfocus(){
+	if(!this.ph_hasEdit)
+		this.value = '';
+}
+function fm_simulate_placeholder_onblur(){
+	if(this.value == "") this.ph_hasEdit = false;
+	if(!this.ph_hasEdit)
+		this.value = this.ph_thePlaceholder;
+}
+function fm_simulate_placeholder_onchange(){
+	this.ph_hasEdit = true;
+}	
+
+function fm_remove_placeholders(){	
+	for(var i=0;i<fm_registered_form_items.length;i++){
+		if(fm_registered_form_items[i].type == 'text'){
+			var textItem = document.getElementById('fm-form-' + fm_registered_form_items[i].formID)[fm_registered_form_items[i].unique_name];
+			textItem.value = fm_registered_form_items[i].extra.value;
+		}
+	}
+}
+
+
+function fm_simulate_HTML5(){
+	
+}
+
+////////////////////////////////////////////////////////////
+//// HELPERS ///////////////////////////////////////////////
+
+function fm_trim(str){
+	return str.replace(/^\s+|\s+$/g,"");
+}
+function fm_fix_str(str){
+	return str.replace(/[\\]/g,'\\\\\\\\').replace(/[']/g,'\\\\\\$&');
+}
+function fm_htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+////////////////////////////////////////////////////////////
+//// FORM ELEMENTS /////////////////////////////////////////
+
+// form element base class
+function fm_base_required_validator(formID, itemID){
+	return (fm_trim(document.getElementById('fm-form-' + formID)[itemID].value) != "");
+}
+function fm_base_get_value(formID, itemID){
+	return fm_trim(document.getElementById('fm-form-' + formID)[itemID].value);
+}
+
+// checkbox
+function fm_checkbox_required_validator(formID, itemID){
+	return document.getElementById('fm-form-' + formID)[itemID].checked;
+}
+
+// list
+function fm_custom_list_required_validator(formID, itemID){
+	var listType = document.getElementById('fm-form-' + formID)[itemID + '-list-style'].value;
+	switch(listType){
+		case "radio":
+			return fm_radio_list_required_validator(formID, itemID);
+		case "checkbox": 
+			return fm_checkbox_list_required_validator(formID, itemID);
+		default:
+			return fm_select_list_required_validator(formID, itemID);
+	}
+	return false;
+}
+function fm_select_list_required_validator(formID, itemID){			
+	return (document.getElementById('fm-form-' + formID)[itemID].value != 0);
+}
+function fm_radio_list_required_validator(formID, itemID){	
+	var radioList = document.getElementById('fm-form-' + formID)[itemID];
+	for(var x=0;x<radioList.length;x++)
+		if(radioList[x].checked == true) return true;		
+	return false;
+}
+function fm_checkbox_list_required_validator(formID, itemID){
+	var count = document.getElementById('fm-form-' + formID)[itemID + '-count'].value;
+	for(var x=0;x<count;x++){
+		if(document.getElementById('fm-form-' + formID)[itemID + '-' + x].checked) return true;
+	}
+	return false;
+}
