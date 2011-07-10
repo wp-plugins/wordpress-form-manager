900d90072714438c906ff3ef5aedae35f97226c0
hoffcamp <hoffcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-10 03:40:46 +0000

    tagged 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-form-manager/tags/1.6.1@407699 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- types/textarea.php
+++ types/textarea.php
@@ -7,15 +7,36 @@ class fm_textareaControl extends fm_controlBase{
 	public function getTypeLabel(){ return __("Text Area", 'wordpress-form-manager'); }
 	
 	public function showItem($uniqueName, $itemInfo){
+		global $fm_display;
+		
 		$elem=array('type' => 'textarea',
 					'attributes' => array('name' => $uniqueName,
 											'id'=> $uniqueName,
-											'style' => "width:".$itemInfo['extra']['cols']."px;height:".$itemInfo['extra']['rows']."px;",
-											'placeholder' => htmlspecialchars($itemInfo['extra']['value']),
+											'style' => "width:".$itemInfo['extra']['cols']."px;height:".$itemInfo['extra']['rows']."px;",											
 											),
-					);					
+					);
+		
+		if(isset($fm_display->currentFormOptions['use_placeholders']) && $fm_display->currentFormOptions['use_placeholders'] === false)
+			$elem['default'] = htmlspecialchars($itemInfo['extra']['value']);			
+		else 
+			$elem['attributes']['placeholder'] = htmlspecialchars(strip_tags($itemInfo['extra']['value']));
+		
 		return fe_getElementHTML($elem);
-	}	
+	}
+	
+	public function showItemSimple($uniqueName, $itemInfo){
+		global $fm_display;
+		
+		$elem=array('type' => 'textarea',
+					'attributes' => array('name' => $uniqueName,
+											'id'=> $uniqueName,
+											),
+					);
+
+		$elem['default'] = htmlspecialchars($itemInfo['extra']['value']);
+		
+		return fe_getElementHTML($elem);
+	}
 	
 	public function itemDefaults(){
 		$itemInfo = array();
@@ -94,7 +115,40 @@ class fm_textareaControl extends fm_controlBase{
 	
 	protected function getPanelKeys(){
 		return array('label','required');
-	}	
+	}
 }
 
+class fm_metaTextareaControl extends fm_textareaControl {
+	public function isSubmissionMeta() { return true; }
+	public function isFormField() { return false; }
+	
+	public function showExtraScripts() { }
+	
+	public function getTypeName(){ return "metatextarea"; }
+	
+	public function getPanelItems($uniqueName, $itemInfo){
+		$arr=array();
+		$arr[] = new fm_editPanelItemBase($uniqueName, 'label', __('Label', 'wordpress-form-manager'), array('value' => $itemInfo['label']));
+		$arr[] = new fm_editPanelItemBase($uniqueName, 'value', __('Default', 'wordpress-form-manager'), array('value' => $itemInfo['extra']['value']));
+		$arr[] = new fm_editPanelItemBase($uniqueName, 'rows', __('Height (in pixels)', 'wordpress-form-manager'), array('value' => $itemInfo['extra']['rows']));
+		$arr[] = new fm_editPanelItemBase($uniqueName, 'cols', __('Width (in pixels)', 'wordpress-form-manager'), array('value' => $itemInfo['extra']['cols']));
+		return $arr;
+	}
+	
+	public function getPanelScriptOptions(){
+		$opt = $this->getPanelScriptOptionDefaults();		
+		$opt['extra'] = $this->extraScriptHelper(array('value'=>'value', 'rows'=>'rows', 'cols'=>'cols'));		
+		return $opt;
+	}
+	
+	public function processPost($uniqueName, $itemInfo){
+		if($_POST[$uniqueName] != null)
+			return strip_tags($_POST[$uniqueName]);
+		return $itemInfo['extra']['value'];
+	}
+	
+	protected function getPanelKeys(){
+		return array('label');
+	}
+}
 ?>
